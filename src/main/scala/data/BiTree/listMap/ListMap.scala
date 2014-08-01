9642eff3d4761d54a36edb95c0e03254ee5266b3
YRGD <yoeluk@gmail.com>
2014-08-01 19:35:42 -0400

    Change name to ListMap

--- src/main/scala/data/BiTree/listMap/ListMap.scala
+++ src/main/scala/data/BiTree/listMap/ListMap.scala
@@ -1,10 +1,10 @@
-package data.BiTree.hashList
+package data.BiTree.listMap
 
-trait HashList[+A, +B] {
+trait ListMap[+A, +B] {
 
   def allKeys: List[A] = {
 
-    def go(m: HashList[A, B], ac: List[A]): List[A] = m match {
+    def go(m: ListMap[A, B], ac: List[A]): List[A] = m match {
       case Empty => Nil
       case EndL(k) => k :: ac
       case Root(l, r) => go(l, ac)
@@ -28,21 +28,21 @@ trait HashList[+A, +B] {
   }
 }
 
-case object Empty extends HashList[Nothing, Nothing]
+case object Empty extends ListMap[Nothing, Nothing]
 
-case class Root[A, B](left: HashList[A, B], right: HashList[A, B]) extends HashList[A, B]
+case class Root[A, B](left: ListMap[A, B], right: ListMap[A, B]) extends ListMap[A, B]
 
-case class EndL[A, B](value: A) extends HashList[A, B]
+case class EndL[A, B](value: A) extends ListMap[A, B]
 
-case class EndR[A, B](value: B) extends HashList[A, B]
+case class EndR[A, B](value: B) extends ListMap[A, B]
 
-case class Fork[A, B](value: A, left: HashList[A, B], right: HashList[A, B]) extends HashList[A, B]
+case class Fork[A, B](value: A, left: ListMap[A, B], right: ListMap[A, B]) extends ListMap[A, B]
 
-object HashList {
+object ListMap {
 
-  def apply[B](pairs: List[(String, B)]): HashList[String, B] = {
+  def apply[B](pairs: List[(String, B)]): ListMap[String, B] = {
 
-    def go(p: List[(String, B)], ac: HashList[String, B]): HashList[String, B] = p match {
+    def go(p: List[(String, B)], ac: ListMap[String, B]): ListMap[String, B] = p match {
       case (a, b) :: xs => ac match {
         case Empty => go(xs, Root(EndL(a), EndR(b)))
         case Root(l, r) => go(xs, Root(Fork(a, l, r), EndR(b)));
@@ -56,7 +56,7 @@ object HashList {
     }
   }
 
-  def foldRight[B](h: HashList[String, B], ac: B)(f: (B, B) => B): B = h match {
+  def foldRight[B](h: ListMap[String, B], ac: B)(f: (B, B) => B): B = h match {
     case Empty => ac
     case EndR(v) => f(v, ac)
     case Root(l, EndR(v)) => f(foldRight(l, ac)(f), f(v, ac))
@@ -64,7 +64,7 @@ object HashList {
     case _ => ac
   }
 
-  def foldLeft[B](h: HashList[String, B], ac: B)(f: (B, B) => B): B = h match {
+  def foldLeft[B](h: ListMap[String, B], ac: B)(f: (B, B) => B): B = h match {
     case Empty => ac
     case EndR(v) => f(v, ac)
     case Root(l, EndR(v)) => foldLeft(l, f(v, ac))(f)
@@ -74,19 +74,19 @@ object HashList {
 
 }
 
-import HashList._
+import ListMap._
 
-object HashListStat extends App {
+object ListMapStat extends App {
 
   val l = List(("two", 2), ("three", 3), ("four", 4))
-  val hl = HashList(l)
+  val lm = ListMap(l)
 
-  println(hl.allKeys)
+  println(lm.allKeys)
 
-  println("sums with foldRight: " + foldRight(hl, 0)((x, y) => x + y))
-  println("sums with foldLeft: " + foldLeft(hl, 0)((x, y) => x + y))
+  println("sums with foldRight: " + foldRight(lm, 0)((x, y) => x + y))
+  println("sums with foldLeft: " + foldLeft(lm, 0)((x, y) => x + y))
 
-  println("value for key(four): " + hl.valForKey("four").getOrElse("not found"))
-  println("value for key(two): " + hl.valForKey("two").getOrElse("not found"))
+  println("value for key(four): " + lm.valForKey("four").getOrElse("not found"))
+  println("value for key(two): " + lm.valForKey("two").getOrElse("not found"))
 
 }
